84f6ba181ebfe000037d1a8f08bd837fcb1273f8
subhrat8 <Subhratkumarsamal1@gmail.com>
2026-03-05 22:52:17 +0530

    Update assistant.js

--- static/assistant.js
+++ static/assistant.js
@@ -5,54 +5,73 @@ const chatBody = document.getElementById("chat-body")
 
 bubble.onclick = () => {
 
-    box.style.display =
-        box.style.display === "flex" ? "none" : "flex"
+box.style.display =
+box.style.display === "flex" ? "none" : "flex"
+
 }
 
 function sendMessage(){
 
-    const message = input.value.trim()
+const message = input.value.trim()
 
-    if(!message) return
+if(!message) return
 
-    addUserMessage(message)
+addUserMessage(message)
 
-    input.value = ""
+input.value = ""
 
-    fetch("/chat",{
-        method:"POST",
-        headers:{
-            "Content-Type":"application/json"
-        },
-        body:JSON.stringify({message:message})
-    })
-    .then(res=>res.json())
-    .then(data=>{
-        addBotMessage(data.reply)
-    })
+fetch("/chat",{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({
+
+message:message,
+
+healthData:window.healthData || null,
+
+hospitals:window.hospitals || null
+
+})
+
+})
+
+.then(res=>res.json())
+
+.then(data=>{
+
+addBotMessage(data.reply)
+
+})
+
 }
 
 function addUserMessage(msg){
 
-    const div = document.createElement("div")
+const div = document.createElement("div")
 
-    div.className = "user-msg"
+div.className = "user-msg"
 
-    div.innerText = msg
+div.innerText = msg
 
-    chatBody.appendChild(div)
+chatBody.appendChild(div)
 
 }
 
 function addBotMessage(msg){
 
-    const div = document.createElement("div")
+const div = document.createElement("div")
 
-    div.className = "bot-msg"
+div.className = "bot-msg"
 
-    div.innerText = msg
+div.innerText = msg
 
-    chatBody.appendChild(div)
+chatBody.appendChild(div)
 
-    chatBody.scrollTop = chatBody.scrollHeight
+chatBody.scrollTop = chatBody.scrollHeight
+
 }
